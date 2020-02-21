779223144e07844604eeb22175f5fecf14279e14
qunfei wu <qunfei.wu@outlook.com>
2020-02-21 11:18:19 +0100

    1. init project with spring boot

---
+++ src/test/java/com/github/stream/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stream.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
